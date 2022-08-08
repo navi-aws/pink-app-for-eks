51112deadc1c77edfec28907575460cc587b2a57
Pro <47291803+navi-aws@users.noreply.github.com>
2022-08-08 11:06:43 +1000

    Delete reset-pin directory

--- reset-pin/shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Running important steps to reset your pin number
-echo 
-echo Pin number will be reset in 30 seconds
-echo
-echo You will be notified via email and SMS
-
-sleep 30
