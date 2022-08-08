1db0fb7eb99c7e44791c0475e425b37e03bda9ee
EC2 Default User <ec2-user@ip-172-31-11-94.ap-southeast-1.compute.internal>
2022-08-08 01:05:23 +0000

    all files

---
+++ reset-pin/shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Running important steps to reset your pin number
+echo 
+echo Pin number will be reset in 30 seconds
+echo
+echo You will be notified via email and SMS
+
+sleep 30
